bf4f56b754abc51f39cd7e4a28575cf45ba6e4fe
Jucelio Farias Coelho <juceliocoelho2022@gmail.com>
2024-09-02 19:08:57 -0300

    Project final

---
+++ settings.gradle.kts
@@ -0,0 +1,23 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "App Food Lanchonete de Sanduíche"
+include(":app")
